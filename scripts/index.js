f5c1beb9effff100fff91caac52fe32eb505df9c
Jorge Huang <jorgehuang27@gmail.com>
2020-04-17 18:14:58 -0400

    Adding recommended readings

--- scripts/index.js
+++ scripts/index.js
@@ -22,6 +22,20 @@ function addArticles() {
     });
 }
 
+function addRecommendedReading() {
+    const links = [
+        '<a target="_blank" href="https://www.amazon.com/gp/product/1593279922/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=1593279922&linkCode=as2&tag=jorgehuang-20&linkId=0e46fa35e55effccb72d6c0511f7229c">Automate the Boring Stuff with Python, 2nd Edition: Practical Programming for Total Beginners</a><img src="//ir-na.amazon-adsystem.com/e/ir?t=jorgehuang-20&l=am2&o=1&a=1593279922" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />',
+    ]
+
+    links.forEach((link) => {
+        const linkContentEl = document.querySelector(".reading .content ul");
+        if (linkContentEl) {
+            linkContentEl.innerHTML += `<li>${link}</li>`;
+        }
+    });
+}
+
 (function () {
     addArticles();
+    addRecommendedReading();
 })();
